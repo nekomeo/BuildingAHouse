2d11bb8ee06a753cfed6e994ad4239c95d94713b
nekomeo <linh.tu545@gmail.com>
2017-08-01 21:51:35 -0700

    Create repo & initial commit

---
+++ BuildingAHouse/BuildingAHouse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuildingAHouse
+//
+//  Created by Elle Ti on 2017-08-01.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
